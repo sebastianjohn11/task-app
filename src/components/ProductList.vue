654d32623458f9629c2f8a69e1f62d54ab4326b8
=sebastian john <=kiran6784@gmail.com>
2022-02-11 12:00:17 +0530

    Added blue color to button style

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button>Add Product</button>
+    <button class="btn-style">Add Product</button>
     <div v-for="(product, index) in productList" :key="index">
       <product-description :product="product" @update-product-list="updateProductList"/>
     </div>
@@ -43,3 +43,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.btn-style {
+  color: blue;
+}
+</style>
