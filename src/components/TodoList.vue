2b7e1c4118dd3743ea4967c1d88aa2afa52f32d9
Sebastian <kiran6784@gmail.com>
2022-02-15 07:14:21 +0530

    basic todo app structure working

---
+++ src/components/TodoList.vue
@@ -0,0 +1,32 @@
+<template>
+   <v-card
+      max-width="450"
+      class="mx-auto"
+    >
+      <v-list>
+        <v-list-item v-for="(todo, index) in todos" :key="index">
+            Task: {{ todo.task }}
+            description: {{ todo.description }}
+            assigned: {{ todo.assignedTo }}
+        </v-list-item>
+      </v-list>
+   </v-card>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data () {
+    return {
+      name: '',
+      todos: [
+        {
+          task: 'task1',
+          description: 'task description',
+          assignedTo: 'John'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/views/Todo.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <h1>Todo App</h1>
+    <todo-list></todo-list>
+</div>
+</template>
+
+<script>
+import TodoList from '../components/TodoList.vue'
+
+export default {
+  name: 'TodoView',
+  components: {
+    TodoList
+  }
+}
+</script>
