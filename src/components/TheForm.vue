dfa36741369634af6c280e61b72a1fa545c78791
Sebastian <kiran6784@gmail.com>
2022-02-16 23:13:39 +0530

    edit is not working

---
+++ src/components/TheForm.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+       <button @click="addList"></button><!--button to display dialoge input texts!-->
+        <dialog open v-if="AddList==true">
+            <form @submit.prevent="submitForm">
+                <div>
+                <label for="task">Task</label>
+                <input type="text" id="task" v-model="gotTask"/>
+                </div>
+                <label for ="description">description</label>
+                <input textarea="text" id="description"  v-model="gotDescription"/>
+                <div>
+                <label for="assiginTo">Assigin to</label>
+                <input type= "text" id="assiginTo" row="3"  v-model="gotAssiginTo"/>
+                </div>
+                <button type="enter">update</button>
+            </form>
+        </dialog>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      gotTask: '',
+      gotDescription: '',
+      gotAssiginTo: ' ',
+      addList: true
+    }
+  },
+  methods: {
+    submitForm () {
+      this.$emit('submit-form', this.gotTask, this.gotDescription, this.gotAssiginTo)
+    }
+  }
+}
+</script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,31 +1,58 @@
 <template>
-   <v-card
-      max-width="450"
-      class="mx-auto"
-    >
-      <v-list>
-        <v-list-item v-for="(todo, index) in todos" :key="index">
-            Task: {{ todo.task }}
-            description: {{ todo.description }}
-            assigned: {{ todo.assignedTo }}
-        </v-list-item>
-      </v-list>
-   </v-card>
-</template>
+  <ul>
+            <v-card
+                max-width="950"
+                class="mx-auto">
+                <v-list>
+                    <v-list-item v-for="(todo, index) in todos" :key="index">
+                        Task: {{ todo.task }}
+                        <br>
+                        description: {{ todo.description }}
+                        <br>
+                        assigned to: {{ todo.assignedTo }}
+                        <br>
+                        <br>
+                        <button @ click="delete">DELETE</button>
+                        <!--  !-->
 
+                    </v-list-item>
+                </v-list>
+                <the-form @submit-form="submittedForm">
+                        </the-form>
+            </v-card>
+  </ul>
+</template>
 <script>
+import TheForm from './TheForm.vue'
 export default {
+  components: {
+    TheForm
+  },
   name: 'TodoList',
   data () {
     return {
-      name: '',
       todos: [
         {
-          task: 'task1',
-          description: 'task description',
-          assignedTo: 'John'
+          id: '',
+          task: '',
+          description: '',
+          assignedTo: ''
         }
       ]
+    }
+  },
+  methods: {
+    submittedForm (gotTask, gotDescription, gotAssiginTo) {
+      const newList = {
+        task: gotTask,
+        description: gotDescription,
+        assignedTo: gotAssiginTo
+      }
+      this.todos.push(newList)
+    },
+    delete () {
+      // eslint-disable-next-line no-self-compare
+      this.todos = this.todos.filter(index => index !== index)
     }
   }
 }
